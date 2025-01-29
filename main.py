a9ef2e621212e51382143e295fdee23009215ee0
jfm57-bath <jfm57@bath.ac.uk>
2025-01-29 21:40:31 +0000

    Added commands for database manipulation

--- main.py
+++ main.py
@@ -2,29 +2,72 @@ import sqlite3
 conn = sqlite3.connect('store')
 print ("Database has been created")
 
-conn.execute("DROP TABLE IF EXISTS pet")
 
-conn.execute("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
+def create_flights():
+    conn.execute("DROP TABLE IF EXISTS flights")
+    conn.execute("CREATE TABLE flights (flight_ID VARCHAR(20), origin VARCHAR(20), destination VARCHAR(20), pilotID VARCHAR(20), departuredate DATE, departuretime VARCHAR(20))")
 
-print ("Table created successfully")
 
-conn.execute("INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES \
-  ('Fluffy','Harold','cat','f',5,'2001-02-04','')")
 
-conn.execute("INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES \
-  ('Claws','Gwen','cat','m',2,'2000-03-17','')")
+def add_flight(parameters):
+    print(str(parameters) + 'parameters')
+    conn.execute("INSERT INTO flights (flight_ID,origin,destination,pilotID,departuredate,departuretime)VALUES \
+  ('BHX455','Birmingham','Ibiza','123','2001-02-04','')")
+    conn.commit()
 
-conn.commit()
-print("Records created successfully")
-print("Total number of rows created :", conn.total_changes)
+def view_schedule(name,surname):
+    if name == '' and surname == '':
+        print('Showing list of pilots')
+        #show list of pilots and prompt to select from list.
+    else:
+        print('Searching records')
 
-cursor = conn.execute("SELECT name,owner,species,sex,checkups,birth,death from pet")
+def assign(pilot_ID,flight_ID):
+    print('Assigning')
 
-for row in cursor:
-   print("name = ", row[0])
-   print("owner = ", row[1])
-   print("species = ", row[2])
-   print("sex = ", row[3])
-   print("checkups = ", row[4])
-   print("birth = ", row[5])
-   print("death = ", row[6], "\n")
+def view_flights():
+    print('viewing flights')
+
+def update_flight():
+    print('updating flights')
+
+
+def commands(letter):
+    #All commands have two options for usage. Either state the command and then it will walk you through the options. Or if you know the innputs already you can enter them.
+    print(letter[0])
+    match letter[0]:
+        case 'ADD':
+            return (add_flight(letter[1]))
+        case 'VIEWFLIGHTS':
+            return (view_flights(letter[1],letter[2]))
+        case 'UPDATE':
+            return (update_flight(letter[1]))
+        case 'ASSIGN':
+            return (assign(letter[1],letter[2]))
+        case 'VIEWSCHEDULE':
+            return (view_schedule(letter[1],letter[2]))
+        case 'VIEWDESTINTATION':
+            return print('hello')
+        case 'UPDATEDESTINATION':
+            return print('test')
+        case 'ADDDESTINATION':
+            return print('test')
+
+def process_command(string):
+    result = string.split()
+    print(result)
+    print(commands(result))
+
+main = True
+
+create_flights()
+
+
+
+
+while main == True:
+    process_command(input())
+    
+
+
+
